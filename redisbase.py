90075b5bb62e425f91228efbad7b929bf2e2bd1a
zhangyifei <zhangyifei@baixing.com>
2015-03-14 18:15:05 +0800

    refine

--- redisbase.py
+++ redisbase.py
@@ -8,6 +8,8 @@
 """Wrap the base operations to redis.
 """
 
+import redis
+
 from rediswrapper import Wrapper
 
 
@@ -16,39 +18,67 @@ class Base(object):
     def __init__(self, name, ttl=-1, host='localhost', port=6379, db=0, **kwargs):
         self.name = name
         self.ttl = ttl
-        self.rdb = Base.instance(host, port, db, **kwargs)
-
-    @staticmethod
-    def instance(host='localhost', port=6379, db=0, **kwargs):
-        return Wrapper(host=host, port=port, db=db, **kwargs)
+        self.client = Wrapper(host=host, port=port, db=db, **kwargs).client
 
     def get_name(self):
         """get the name of the key"""
         return self.name
 
+    def set_name(self, name):
+        """set the name of the key"""
+        self.name = name
+
     def delete(self):
-        """delete the key"""
-        return self.rdb.delete(self.get_name())
+        """delete the key.
+
+        Return:
+        + 0, the key doesn't exist before
+        + 1, the key existed before and is deleted successfully
+        """
+        return self.client.delete(self.get_name())
 
     def exists(self):
         """detect whether the key exists"""
-        return self.rdb.exists(self.get_name())
+        return self.client.exists(self.get_name())
 
     def get_ttl(self):
         """get the ttl of the key"""
-        return self.rdb.ttl(self.get_name())
+        return self.client.ttl(self.get_name())
 
     def set_ttl(self, ttl=0):
         """set the ttl of the key"""
-        return (ttl > 0) && self.rdb.expire(self.get_name(), ttl);
+        self.ttl = ttl
+        
+        return (ttl > 0) and self.client.expire(self.get_name(), ttl)
 
     def refresh(self):
         """refresh the ttl of the key"""
         return self.set_ttl(self.ttl)
 
-    def rename(self, old, new):
-        """rename the key 'old' to 'new'"""
+    def rename(self, name):
+        """rename the key"""
+        self.set_name(name)
+        
         try:
-            return self.rdb.rename(old, new)
+            return self.client.rename(self.get_name(), name)
+        except redis.ResponseError:
+            # the key may not exist
+            return True
         except Exception:
             return False
+
+
+if __name__ == '__main__':
+    import sys
+    
+    base = Base('cache', 30)
+    base_1 = Base('cache', 30)
+    print('id of base is {0}, id of base_1 is {1}'.format(id(base.client), id(base_1.client)))
+
+    print('get_name: {0}'.format(base.get_name()))
+    print('delete cache: {0}'.format(base.delete()))
+    print('Does the key "cache" exists? {0}'.format(base.exists()))
+    print('ttl: {0}'.format(base.get_ttl()))
+    print('set ttl: {0}'.format(base.set_ttl(10)))
+    print('refresh: {0}'.format(base.refresh()))
+    print('rename: {0}'.format(base.rename('new')))
