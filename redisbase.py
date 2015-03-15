f37bdeca97ecfc97deeddfa7535b1b5bbfe37188
zhangyifei <zhangyifei@baixing.com>
2015-03-15 19:10:10 +0800

    add the string operations

--- redisbase.py
+++ redisbase.py
@@ -15,18 +15,49 @@ from rediswrapper import Wrapper
 
 class Base(object):
     """"""
+    PREFIX = {
+        'base': 'base',
+        'rstring': 'str',
+        'rhash': 'hash',
+        'rlist': 'list',
+        'rset': 'set',
+        'rzset': 'zset'
+    }
+    
     def __init__(self, name, ttl=-1, host='localhost', port=6379, db=0, **kwargs):
-        self.name = name
+        self.host = host
+        self.port = port
+        self.db = db
+        self.kwargs = kwargs
+        
+        self.name_ud = name     # user defined name
+        self._set_name(name)    # stored name
         self.ttl = ttl
-        self.client = Wrapper(host=host, port=port, db=db, **kwargs).client
+        self.client = Wrapper(self.host, self.port, self.db, **self.kwargs).client
 
+    def get_redis_config(self):
+        """get the config of redis"""
+        return self.host, self.port, self.db, self.kwargs
+    
+    def _get_prefix(self):
+        """get the prefix"""
+        return Base.PREFIX[type(self).__name__.lower()]
+        
     def get_name(self):
-        """get the name of the key"""
-        return self.name
+        """get the user-defined name of the key"""
+        return self.name_ud
 
     def set_name(self, name):
-        """set the name of the key"""
-        self.name = name
+        """set the user-defined name of the key"""
+        self.name_ud = name
+
+    def _get_name(self):
+        """get the stored name"""
+        return self.name_stored
+        
+    def _set_name(self, name):
+        """set the stored name of the key"""
+        self.name_stored = '{0}:{1}'.format(self._get_prefix(), self.name_ud)
 
     def delete(self):
         """delete the key.
@@ -35,48 +66,65 @@ class Base(object):
         + 0, the key doesn't exist before
         + 1, the key existed before and is deleted successfully
         """
-        return self.client.delete(self.get_name())
+        return self.client.delete(self._get_name())
 
     def exists(self):
         """detect whether the key exists"""
-        return self.client.exists(self.get_name())
+        return self.client.exists(self._get_name())
 
     def get_ttl(self):
         """get the ttl of the key"""
-        return self.client.ttl(self.get_name())
+        return self.client.ttl(self._get_name())
 
+    def get_default_ttl(self):
+        """get the original ttl"""
+        return self.ttl
+    
     def set_ttl(self, ttl=0):
         """set the ttl of the key"""
         self.ttl = ttl
         
-        return (ttl > 0) and self.client.expire(self.get_name(), ttl)
+        return (ttl > 0) and self.client.expire(self._get_name(), ttl)
 
     def refresh(self):
         """refresh the ttl of the key"""
-        return self.set_ttl(self.ttl)
+        return self.set_ttl(self.get_default_ttl())
 
     def rename(self, name):
         """rename the key"""
         self.set_name(name)
+        old_name = self._get_name()
+        self._set_name(name)
+        new_name = self._get_name()
         
         try:
-            return self.client.rename(self.get_name(), name)
+            return self.client.rename(old_name, new_name)
         except redis.ResponseError:
             # the key may not exist
             return True
         except Exception:
             return False
 
+    def get_type(self):
+        """determine the type of the key"""
+        return self.client.type(self._get_name())
+
 
 if __name__ == '__main__':
     base = Base('cache', 30)
     base_1 = Base('cache', 30)
-    print('id of base is {0}, id of base_1 is {1}'.format(id(base.client), id(base_1.client)))
+    print('id of base is {0}, id of base_1 is {1}\n'.format(id(base.client), id(base_1.client)))
 
+    print('prefix: {0}'.format(base._get_prefix()))
+    print('redis config: {0}'.format(base.get_redis_config()))
     print('get_name: {0}'.format(base.get_name()))
     print('delete cache: {0}'.format(base.delete()))
     print('Does the key "cache" exists? {0}'.format(base.exists()))
     print('ttl: {0}'.format(base.get_ttl()))
+    print('default ttl: {0}'.format(base.get_default_ttl()))
     print('set ttl: {0}'.format(base.set_ttl(10)))
+    print('default ttl: {0}'.format(base.get_default_ttl()))
     print('refresh: {0}'.format(base.refresh()))
     print('rename: {0}'.format(base.rename('new')))
+    print('type: {0}'.format(base.get_type()))
+    print('sotred_name: "{0}"\n'.format(base._get_name()))
---
+++ redisstring.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+
+# --------------------------------------------
+# Author: flyer <flyer103@gmail.com>
+# Date: 2015/03/15 16:39:52
+# --------------------------------------------
+
+"""Wrap the string operations to redis.
+"""
+
+import redis
+
+from redisbase import Base
+
+
+class RString(Base):
+    """"""
+    def __init__(self, name, ttl=-1, host='localhost', port=6379, db=0, **kwargs):
+        super(RString, self).__init__(name, ttl, host, port, db, **kwargs)
+
+    def get(self):
+        """get the value of the key"""
+        return self.client.get(self._get_name())
+
+    def mget(self, lst=[]):
+        """get the values of all the given keys"""
+        tmp_lst = ['{0}:{1}'.format(self._get_prefix(), key) for key in lst]
+        try:
+            return self.client.mget(tmp_lst)
+        except redis.ResponseError:
+            return []
+        except Exception:
+            return None
+
+    def set(self, value):
+        """set the string value of the key"""
+        self.client.set(self._get_name(), value, self.get_default_ttl())
+
+    def mset(self, dic={}):
+        """set multiple keys to multiple values"""
+        tmp_dict = {'{0}:{1}'.format(self._get_prefix(), k): v for k, v in dic.items()}
+        
+        try:
+            ret = self.client.mset(tmp_dict)
+        except redis.ResponseError:
+            # `tmp_dict` may be empty
+            ret = True
+        except Exception:
+            ret = False
+
+        host, port, db, kwargs = self.get_redis_config()
+        for key in dic:
+            RString(key, self.get_default_ttl(), host, port, db, **kwargs).refresh()
+            
+        return ret
+
+    def getset(self, value):
+        """set the string value of the key and return its old value"""
+        ret = self.client.getset(self._get_name(), value)
+        self.refresh()
+
+        return ret
+ 
+    def incr_by(self, value=1):
+        """increment the integer value of the key by the given amount"""
+        return self.client.incrby(self._get_name(), value)
+
+    def incr(self):
+        """increment the integer value of the key by 1"""
+        return self.incr_by()
+
+    def decr_by(self, value=1):
+        """decrement the integer value of the key by the given number"""
+        return self.client.decr(self._get_name(), value)
+
+    def decr(self):
+        """decrement the integer value of the key by 1"""
+        return self.decr_by()
+
+    def get_length(self):
+        """get the length of the value"""
+        return self.client.strlen(self._get_name())
+
+    
+if __name__ == '__main__':
+    rdb = RString('cache', 10)
+
+    print('prefix: "{0}"\n'.format(rdb._get_prefix()))
+
+    print('name of the key: "{0}"'.format(rdb.get_name()))
+    print('origin ttl: {0}'.format(rdb.get_default_ttl()))
+    print('value: {0}'.format(rdb.get()))
+    print('values of keys: {0}\n'.format(rdb.mget()))
+
+    print('set key "cache" to "flyer"')
+    rdb.set('flyer')
+    print('value: {0}'.format(rdb.get()))
+    print('length: {0}'.format(rdb.get_length()))
+    print('ttl: {0}\n'.format(rdb.get_ttl()))
+
+    print('multi set')
+    print('empty dict: {0}'.format(rdb.mset()))
+    print('normal dict: {0}'.format(rdb.mset({'name': 'flyer', 'gender': 'male'})))
+    print('values: {0}\n'.format(rdb.mget(['name', 'gender'])))
+
+    print('getset: {0}'.format(rdb.getset('test')))
+    print('ttl: {0}\n'.format(rdb.get_ttl()))
+
+    print('set key "cache" to 1 and increment by 10')
+    rdb.set(1)
+    print('length: {0}'.format(rdb.get_length()))
+    rdb.incr_by(10)
+    print('value of key "cache": {0}'.format(rdb.get()))
+    print('length: {0}'.format(rdb.get_length()))
+    print('after +1, value of key "cache": {0}'.format(rdb.incr()))
+    print('decrement by 3, value: {0}'.format(rdb.decr_by(3)))
+    print('after -1, value: {0}\n'.format(rdb.decr()))
